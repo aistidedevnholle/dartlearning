016681c82eeeaefe01442fcfecf7b93352e13f01
Aristide Nholle <maxweofficiel.20@gmail.com>
2021-02-02 14:37:03 +0000

    some change

--- map.dart
+++ map.dart
@@ -14,16 +14,24 @@ main(List<String> args) {
   Map<String, dynamic> infoAlex = new Map<String, dynamic>();
   Map<String, dynamic> infoAlex2 = new Map();
 
-  Map<String, dynamic> infoAlex3 = {};
+  Map<int, dynamic> infoAlex7 =
+      {}; //CLE: ENTIER , VALEUR PEUT int double, string
 
+  Map<String, dynamic> infoAlex3 =
+      {}; // CLE: CHAINE DE CARACTERE , VALEUR PEUT int double, string
   //CONNAITRE LE NOMBRE DELEMENT DANS NOTE MAP
   print(
       'VALUE OF MAP ${infoAlex.length} --- ${infoAlex2.length} ----${infoAlex3.length} ');
 
   //DECLARER UNE MAP AVEC DES ELEMENT PAR DEFAUT
 
-  var info = {'name': 'Alex', 'age': 4};
-
+  var info = {
+    'name': 'Alex',
+    'age': 4,
+    1: 'dog',
+    1.2: 17
+  }; //CLE VALEUR PEUVENT ETRE DE TOUT TYPE int string, double...
+  //INFO ALEX
   Map<String, dynamic> infoOfAlex = {};
 
   //AJOUTER DES ELEMENTS DANS UNE MAP
@@ -35,7 +43,10 @@ main(List<String> args) {
 
   infoOfAlex['civilite'] = "Mr";
 
+  //METTRE A JOUR OU MODIFIER UNE VALEUR
   infoOfAlex['name'] = 'Kouassi';
+
+  infoOfAlex.update('civilite', (dynamic) => "Mme");
 
   //2 AJOUTER PLUSIEURS ELEMENT
   infoOfAlex.addAll({'age': 17, 'taille': 1.2, 'age2': 1.2});
@@ -50,9 +61,18 @@ main(List<String> args) {
 
   //SUPPRIMER COMPLETEMENT
 
-  infoOfAlex.clear();
+  //infoOfAlex.clear();
 
   print("INFO ALEX: $infoOfAlex");
 
-  //AJOUTER PLUSIEURS ELEMENTS DANS UNE MAP
+  //CONNAITRE LES VALEURS DE NOTRE MAP
+  var value = infoOfAlex.values;
+
+  print('values: $value');
+
+  //CONNAITRE LES KEYS:CLE DE NOTRE MAP
+
+  var keys = infoOfAlex.keys;
+
+  print('values: $keys');
 }
